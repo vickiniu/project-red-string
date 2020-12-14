f3122be3399b7f90465115ea66c47b4742233bd4
Vicki Niu <vicki.niu@gmail.com>
2020-12-14 17:48:33 -0600

    web: add in frontend

---
+++ web/src/App.tsx
@@ -0,0 +1,33 @@
+import "./App.css";
+import Search from "./components/Search";
+import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
+
+function App() {
+    return (
+        <div className="home">
+            <Container>
+                <h1>Project Red String</h1>
+                <h3>Some description goes here.</h3>
+            </Container>
+            <Container>
+                <p>Search for a candidate or contributor</p>
+                <Search />
+                <p>or, browse by category</p>
+                <Row>
+                    <Col>
+                        <Button>City Council Members</Button>
+                    </Col>
+                    <Col>
+                        <Button>Category #2</Button>
+                    </Col>
+                    <Col>
+                        <Button>Category #3</Button>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
+}
+
+export default App;
---
+++ web/src/components/Search.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+
+interface ResultsListProps {
+    data: SearchResult[] | undefined;
+}
+
+interface SearchResult {
+    id: string;
+    firstName: string;
+    lastName: string;
+}
+
+const ResultsList = (props: ResultsListProps) => {
+    const { data } = props;
+    return data ? (
+        <ul>
+            {data.map((d: SearchResult) => {
+                return <li key={d.id}>{d.firstName + " " + d.lastName}</li>;
+            })}
+        </ul>
+    ) : (
+        <></>
+    );
+};
+
+const Search = () => {
+    const [results, setResults] = useState<SearchResult[]>();
+
+    function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+        console.log(event.target.value);
+        // TODO(vicki): put in env variable for host
+        fetch("http://localhost:8080/search-individuals", {
+            method: "POST",
+            body: JSON.stringify({
+                query: event.target.value,
+            }),
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                console.log("data: ", data);
+                let transformedResults: SearchResult[] = data?.map((d: any) => {
+                    return {
+                        id: d?.id,
+                        firstName: d?.first_name,
+                        lastName: d?.last_name,
+                    };
+                });
+                setResults(transformedResults);
+            })
+            .catch((err) => {
+                console.log("error: ", err);
+            });
+    }
+
+    return (
+        <React.Fragment>
+            <div className="search">
+                <Form.Control
+                    placeholder="Search by name"
+                    onChange={handleChange}
+                />
+                <ResultsList data={results} />
+            </div>
+        </React.Fragment>
+    );
+};
+
+export default Search;
